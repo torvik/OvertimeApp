0785041ad69c7d7b276abedd34f235cd14bfedb2
torvik <pless.victor@gmail.com>
2017-08-02 20:25:29 +0000

    s4a13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802022719) do
+ActiveRecord::Schema.define(version: 20170802190124) do
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,7 +38,5 @@ ActiveRecord::Schema.define(version: 20170802022719) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-
 
 end
