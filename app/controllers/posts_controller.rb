4f2d48efc3547535a4ff6ade5aa52d54ff20743f
torvik <pless.victor@gmail.com>
2017-08-02 20:53:44 +0000

    s4a14

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,32 @@
 class PostsController < ApplicationController
-
+    before_action :set_post, only: [:show]
     def index
-        
+      @posts = Post.all
+    end
+
+    def show
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+         @post = Post.new(params.require(:post).permit(:date, :rationale))
+
+        if @topic.save
+         redirect_to post_path(@post), notice: 'Post was successfully created.'
+        else
+         render :new
+        end
+    end
+    
+    private
+
+    def set_post
+      @post = Post.find(params[:id])  
+      #@post = Post.friendly.find(params[:id])        
+
     end
 
 end
