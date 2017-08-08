6f96f845ebecebdb34c8b507da8f144a9c5452fe
torvik <pless.victor@gmail.com>
2017-08-08 21:06:17 +0000

    s1567

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #validates_presence_of :first_name, :last_name
+  #validates_presence_of :first_name, :last_name, :phone
+
+   PHONE_REGEX = /^[0-9]*/
+   validates_format_of :phone, with: PHONE_REGEX
 
     def full_name
        last_name.upcase + ", " + first_name.upcase
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-@user = User.create(email: "teste@teste.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell")
-@admin = AdminUser.create(email: "admin@admin.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell")
+@user = User.create(email: "teste@teste.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell", phone: "5521995629323")
+@admin = AdminUser.create(email: "admin@admin.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell", phone: "5521995629323")
 
 puts "2 user created"
 
