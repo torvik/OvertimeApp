19ffde022d010a1bdcf77e2f4c4ac95fee4e265e
torvik <pless.victor@gmail.com>
2017-08-08 13:19:29 +0000

    s11a38

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803131850) do
+ActiveRecord::Schema.define(version: 20170808130459) do
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170803131850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 @user = User.create(email: "teste@teste.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell")
+@admin = AdminUser.create(email: "admin@admin.com", password: "123456", password_confirmation: "123456", first_name: "Jon", last_name: "Dell")
 
-puts "1 user created"
+puts "2 user created"
 
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id )
