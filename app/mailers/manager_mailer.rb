c3f6d33dc4839157d47168b9f0f4f727ef48ea48
torvik <pless.victor@gmail.com>
2017-08-17 18:45:02 +0000

    s20a92

---
+++ app/mailers/manager_mailer.rb
@@ -0,0 +1,9 @@
+class ManagerMailer < ApplicationMailer
+   
+    def email manager
+        @manager = manager
+        mail(to: @manager.email, subject: 'Daily Overtime Request Email')
+    end
+
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,4 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_options = {from: 'no-reply@example.com'}
 end
