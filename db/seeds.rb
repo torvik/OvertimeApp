8be9bd29fdbf02de63cd5189d7fda1cf41e5a1be
torvik <pless.victor@gmail.com>
2017-08-17 19:50:55 +0000

    cor

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,12 @@
 
 puts "2 user created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 Audit Logs have been created"
+
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur 
     adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
@@ -14,8 +20,3 @@ end
 
 puts "100 Posts have been created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 Audit Logs have been created"
